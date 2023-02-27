0a2a97ef61f712f4fe03cc4394421810b12523fc
abemaru <ch4nge.lightening@gmail.com>
2023-02-27 23:42:56 +0900

    単純なカウントダウン実装完了

---
+++ vue-timer/src/components/Timer.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { ref, onMounted } from 'vue'
+
+export default {
+    setup() {
+        const remainingTime = ref<number>(60)
+
+        const startTimer = () => {
+            setInterval(() => {
+                remainingTime.value--
+            }, 1000)
+        }
+
+        onMounted(() => {
+            startTimer()
+        })
+
+        return {
+            remainingTime
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="timer">
+        <h1>{{ remainingTime }}</h1>
+    </div>
+</template>
